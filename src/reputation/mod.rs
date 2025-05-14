bdfa6994f5bf977187742322d195bc71f8a8ce56
Matt Faherty <matt.faherty@gmail.com>
2025-05-14 19:37:30 -0400

    refactor: remove ReputationOperator implementation from reputation_operator.rs; streamline codebase by eliminating unused functionality

---
+++ src/reputation/mod.rs
@@ -0,0 +1,33 @@
+// src/reputation/mod.rs
+pub mod reputation_operator;
+pub mod reputation_executor;
+pub mod reputation_director;
+pub mod reputation_supervisor;
+pub mod reputation_coordinator;
+pub mod reputation_orchestrator;
+pub mod reputation_engine;
+pub mod reputation_processor;
+pub mod reputation_manager;
+pub mod reputation_worker;
+pub mod reputation_client;
+pub mod reputation_assigner;
+pub mod reputation_distributor;
+pub mod reputation_allocator;
+pub mod reputation_balancer;
+pub mod reputation_server;
+pub mod reputation_router;
+pub mod reputation_optimizer;
+pub mod reputation_controller;
+pub mod reputation_analyst;
+pub mod reputation_strategist;
+pub mod reputation_planner;
+pub mod reputation_scheduler;
+pub mod reputation_dispatcher;
+pub mod reputation_handler;
+pub mod reputation_metrics;
+pub mod reputation_aggregator;
+pub mod reputation_service;
+pub mod reputation_cache;
+
+// TODO: Re-export key public types from these submodules if needed for easier access
+// e.g., pub use reputation_manager::ReputationManager; 
